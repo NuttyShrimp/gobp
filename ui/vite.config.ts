d87ecb9105d8dea520f8fd30740c40cf3ef14ab5
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-04-13 13:57:40 +0200

    chore: update ui packages

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -2,6 +2,7 @@ import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig(({ mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd(), "SENTRY") };
@@ -13,6 +14,7 @@ export default defineConfig(({ mode }) => {
       sourcemap: true,
     },
     plugins: [
+      tailwindcss(),
       react(),
       sentryVitePlugin({
         org: "",
