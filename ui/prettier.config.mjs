b1eef467f6929f1ea1788802a0248c10d3fd02cf
NuttyShrimp <jan.lecoutere@hotmail.be>
2025-06-25 15:40:05 +0200

    feat(ui): add tailwind class sorting plugin

---
+++ ui/prettier.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+const config = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  useTabs: false,
+  singleQuote: false,
+  trailingComma: "none",
+  printWidth: 120,
+  bracketSameLine: false,
+  endOfLine: "lf"
+};
+
+export default config;
